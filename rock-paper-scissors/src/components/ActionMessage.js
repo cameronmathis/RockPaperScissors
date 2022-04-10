4d0a45d3b0656a36159a3581aa0186da182b2f4a
cameronmathis <cameronmathis@users.noreply.github.com>
2022-04-10 18:54:54 +0000

    deploy: 73b8aa83b0551d90e29029c76a743c947045279b

--- rock-paper-scissors/src/components/ActionMessage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import css
-import "./css/ActionMessage.css";
-
-function ActionMessage() {
-  return <p className="actionMessage">Make your move.</p>;
-}
-
-export default ActionMessage;
